c9c2b3ef873daaa1613ec3c7dfe5287028f8dd27
nafihpp <nafihpp@gmail.com>
2023-06-11 19:55:56 +0530

    bug-fix

--- server/controller/ProductController.js
+++ server/controller/ProductController.js
@@ -1,23 +1,17 @@
 const Products = require("../data/dummy.json");
 
-const getAllProducts = async (req, res) => {
-    try {
-        console.loog(Products);
-        return res.status(200).json(Products);
-    } catch (error) {
-        res.status(400).json({
-            message: error,
-        });
-    }
+const getAllProducts = (req, res) => {
+    return res.status(200).json(Products);
 };
 
-const productDetails = async (req, res) => {
-    try {
-        console.log(Products, "==prod");
-        return res.status(200).json({ message: "successfully added task" });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+const productDetails = (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    const CurrentProduct = Products.find((product) => product.id == id);
+    if (CurrentProduct) {
+        return res.status(200).json(CurrentProduct);
     }
+    return res.status(404).json({ message: "product Not Found" });
 };
 
 module.exports = {
